1114208fa0300d37731c402f667fef64296ef81f
Hackathon66 <115601899+Hackathon66@users.noreply.github.com>
2023-09-13 13:42:06 +0300

    Add files via upload

---
+++ mymandaziwebsite.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MANDAZI</title>
+<style >
+    h1{
+        font-family: bold;
+
+    }
+    img{
+        max-width: 100%,
+    }
+    img{
+        border: 15px solid;
+border-image: url(/img/border-image.png) 30 / 15px / 0px stretch;
+    filter: saturate(200%);
+    
+    }
+    .mandazi {
+            margin: 20px;
+        }
+        h2 {
+            font-size: 1.2em;
+        }
+
+        button {
+            padding: 10px 20px;
+           background: linear-gradient(176deg, #580D0D 0%, #fff 100%);
+            color: white;
+            border: none;
+            cursor: pointer;
+            font-weight: bold;
+        }
+        body{
+            background: linear-gradient(176deg, #580D0D 0%, #fff 100%);
+
+        }
+</style>
+</head>
+<body >
+<CENTER>
+
+    <h1> WELCOME TO MY MANDAZI HOTEL</h1>
+
+<h2>MY MENU</h2>
+<UL><BR>
+    <LI>Doughnut</LI>
+    <li>Ngumu</li>
+    <li>KDF</li>
+    <li>Mahamri</li>
+</UL>
+</BR>
+
+
+
+ <div class="mandazi">
+        <img src="C:\Users\User\Downloads\My mandazi website.html\MANDAZI PIC.jpeg" alt="Mandazi Image"width="300">
+        <h3>Price: $100</h3>
+        <button>CLICK TO BUY MANDAZI</button>
+    </div>
+
+    <div class="mandazi">
+        <img src="C:\Users\User\Downloads\My mandazi website.html\Kdf.jpeg" alt="Kdf Image"width="300">
+        <h3>Price: $100</h3>
+        <button>CLICK TO BUY KDF</button>
+    </div>
+      <div class="mandazi">
+        <img src="C:\Users\User\Downloads\My mandazi website.html\download.jpeg "width="300">
+        <h3>Price: $200</h3>
+        <button>CLICK TO BUY MAHAMRI</button>
+    </div>
+
+</div>
+      <div class="mandazi">
+        <img src="C:\Users\User\Downloads\My mandazi website.html\NGUMU.jpeg"width="300">
+        <h3>Price: $200</h3>
+        <button>CLICK TO BUY NGUMU</button>
+    </div>
+
+
+
+</body>
+</html>
